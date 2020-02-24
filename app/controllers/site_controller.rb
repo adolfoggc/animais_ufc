a7b04f5ea5d4be8845ce5a85f28f4ec4853a6180
Carlos Adolfo <adolfo.ggc@gmail.com>
2020-02-24 10:07:58 -0300

    configurações importadas do ApplicationHelper

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -1,4 +1,7 @@
 class SiteController < ApplicationController
+	include ApplicationHelper
+  
   def index
+  	@sys_conf = get_sys_config
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+	
+	def get_sys_config
+		conf = SysConfiguration.first
+	rescue
+		conf = SysConfiguration.new(name: 'System Name', initials: 'SN')
+	ensure
+		return conf
+	end
+
 end

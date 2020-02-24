4ad1ef340b92fd5775e7d1e5fead54b58804cd48
Carlos Adolfo <adolfo.ggc@gmail.com>
2020-02-24 15:47:43 -0300

    seeds

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,9 +2,9 @@ module ApplicationHelper
 	
 	def get_sys_config
 		conf = SysConfiguration.first
-	rescue
-		conf = SysConfiguration.new(name: 'System Name', initials: 'SN')
-	ensure
+		if(conf.nil?)
+			conf = SysConfiguration.new(name: 'System Name', initials: 'SN')
+		end
 		return conf
 	end
 

b2cdf195569bdddf71f240357ed524f87aeb06eb
Carlos Adolfo <adolfo.ggc@gmail.com>
2020-03-03 21:32:09 -0300

    controller data in members

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,12 +1,19 @@
 class MembersController < ApplicationController
+  include ApplicationHelper
+	before_action :authenticate_admin!
+	layout 'dashboard'
+
   def index
+  	@sys_conf = get_sys_config
   	@admins = Admin.all
   end
 
   def new
+  	@sys_conf = get_sys_config
   	@admin = @admin.new
   end
 
   def delete
+  	@sys_conf = get_sys_config
   end
 end
